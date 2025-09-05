17bbc8928097725807c9efd4be01833850b76be9
Cyan <hoony6134@gmail.com>
2025-09-05 20:08:29 +0900

    chore: file structure

---
+++ CodiCue/StructureView.swift
@@ -0,0 +1,18 @@
+//
+//  StructureView.swift
+//  CodiCue
+//
+//  Created by 임정훈 on 9/5/25.
+//
+
+import SwiftUI
+
+struct StructureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StructureView()
+}
