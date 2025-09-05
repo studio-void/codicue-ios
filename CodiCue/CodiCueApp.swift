8740e38ecea82fb8eeb910cc739cada4ca528724
Cyan <hoony6134@gmail.com>
2025-09-05 19:52:22 +0900

    chore: boilerplate

---
+++ CodiCue/CodiCueApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodiCueApp.swift
+//  CodiCue
+//
+//  Created by 임정훈 on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct CodiCueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
