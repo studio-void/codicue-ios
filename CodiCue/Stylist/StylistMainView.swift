453807a41814b1fa330a3fdfb852f50d14cdb63f
Yeeun Park <yetwin05@gm.gist.ac.kr>
2025-09-21 17:52:33 +0900

    chore: add stylist main structure

---
+++ CodiCue/Stylist/StylistMainView.swift
@@ -0,0 +1,18 @@
+//
+//  StylistMainView.swift
+//  CodiCue
+//
+//  Created by Yeeun on 9/21/25.
+//
+
+import SwiftUI
+
+struct StylistMainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StylistMainView()
+}
