39af674c1c58b00cd73d44b0f40b4b3e34f2454f
Cyan <hoony6134@gmail.com>
2025-09-22 18:12:42 +0900

    feat: stylist list on home (with API call)

---
+++ CodiCue/Model/StylistInfo.swift
@@ -0,0 +1,21 @@
+//
+//  StylistInfo.swift
+//  CodiCue
+//
+//  Created by 임정훈 on 9/22/25.
+//
+
+import Foundation
+
+struct StylistInfo: Hashable, Identifiable {
+    var id: Int
+    var name: String
+    var rating: Double
+    var reviewCount: Int
+    var isVerified: Bool
+    var introduction: String
+    var profileImageUrl: String
+    var specialtyStyles: [String]
+    var createdAt: Date
+    var updatedAt: Date
+}
